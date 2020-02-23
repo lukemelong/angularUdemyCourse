b8bbd3ecc69180ed20513ba2d5df2e4be256a325
lukemelong <luke.melong13@gmail.com>
2020-02-23 09:17:24 -0400

    Section 9 Start

--- udemy-app/src/app/app.module.ts
+++ udemy-app/src/app/app.module.ts
@@ -4,38 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
-=======
-import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless.directive';
->>>>>>> 94f16beb2fa745fc199999d91f187c80f614f481
+import { AccountComponent } from './account/account.component';
+import { NewAccountComponent } from './new-account/new-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective
-=======
-    BasicHighlightDirective,
-    BetterHighlightDirective,
-    UnlessDirective
->>>>>>> 94f16beb2fa745fc199999d91f187c80f614f481
+    AccountComponent,
+    NewAccountComponent
   ],
   imports: [
     BrowserModule,
