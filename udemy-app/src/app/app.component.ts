e2b62f679762f25e7640ff8a79847dc59752d0e3
lukemelong <luke.melong13@gmail.com>
2020-02-23 17:49:55 -0400

    Section 10 Start

--- udemy-app/src/app/app.component.ts
+++ udemy-app/src/app/app.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { UsersService } from './user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
+  loadedFeature = 'recipe';
 
-  constructor(private usersService: UsersService){}
-
-  activeUsers: String[] = [];
-  inactiveUsers: String[] = [];
-  
-  ngOnInit() {
-    this.activeUsers = this.usersService.activeUsers;
-    this.inactiveUsers = this.usersService.inactiveUsers;
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
   }
-} 
+}
