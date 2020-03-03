0895509d980ecb0699804635b07bc167258ca286
lukemelong <luke.melong13@gmail.com>
2020-03-03 18:53:58 -0400

    Section 16 Complete

--- udemy-app/src/app/app.component.ts
+++ udemy-app/src/app/app.component.ts
@@ -1,25 +1,9 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserService } from './user.service';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated: boolean = false;
-  private activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivate => {
-      this.userActivated = didActivate;
-    })
-  }
-
-  ngOnDestroy(){
-    this.activatedSub.unsubscribe();
-  }
+export class AppComponent {
 }
--- udemy-app/src/app/user.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs'
-
-@Injectable({providedIn: 'root'})
-export class UserService{
-    activatedEmitter = new Subject<boolean>();
-}
