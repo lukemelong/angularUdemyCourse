9acc4e397b997a29589ac306335171dc335f2834
lukemelong <luke.melong13@gmail.com>
2020-03-03 17:26:52 -0400

    Sectrion 12 Completed: Course Project Routes

--- udemy-app/src/app/app-routing.module.ts
+++ udemy-app/src/app/app-routing.module.ts
@@ -1,43 +1,26 @@
 import { NgModule } from "@angular/core";
+import { RecipesComponent } from "./recipes/recipes.component";
 import { Routes, RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
-import { UsersComponent } from "./users/users.component";
-import { UserComponent } from "./users/user/user.component";
-import { ServersComponent } from "./servers/servers.component";
-import { ServerComponent } from "./servers/server/server.component";
-import { EditServerComponent } from "./servers/edit-server/edit-server.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { AuthGuard } from "./auth-guard.service";
-import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
-import { ErrorPageComponent } from "./error-page/error-page.component";
-import { ServerResolver } from "./servers/server/server-resolver.service";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipesStartComponent } from "./recipes/recipes-start/recipes-start.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes = [
-    { path: '', component: HomeComponent } , 
-    { path: 'users', component: UsersComponent, 
-        children: [
-            { path: ':id/:name', component: UserComponent }, 
-        ]
-    },
-    { path: 'servers', canActivate: [AuthGuard], component: ServersComponent, 
-        children: [  
-            { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
-            { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
-        ]
-    },
-    { path: 'not-found',component: ErrorPageComponent, data: {message: "Page not found!"}},
-    // { path: 'not-found',component: PageNotFoundComponent },
-    { path: '**', redirectTo: '/not-found' }
-  ];
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: RecipesStartComponent},
+        { path: 'new', component: RecipeEditComponent },
+        { path: ':id', component: RecipeDetailComponent},
+        { path: ':id/edit', component: RecipeEditComponent}
+    ]},
+    { path: 'shopping-list', component: ShoppingListComponent},
+]
+
 @NgModule({
-    imports: [
-        RouterModule.forRoot(appRoutes)
-        // RouterModule.forRoot(appRoutes, {useHash: true})
-    ],
-    exports: [
-        RouterModule
-    ]
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {
+export class AppRoutingModule{
 
 }
--- udemy-app/src/app/app.module.ts
+++ udemy-app/src/app/app.module.ts
@@ -2,40 +2,41 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth-guard.service';
-import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { ServerResolver } from './servers/server/server-resolver.service';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent,
-    PageNotFoundComponent,
-    ErrorPageComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    DropdownDirective,
+    RecipesStartComponent,
+    RecipeEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
+  providers: [ShoppingListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
